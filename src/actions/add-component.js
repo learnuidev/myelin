5d5402507a56f39461753caa63c8d4bed39dc8f4
learnuidev <learnuidev@gmail.com>
2024-07-25 16:31:16 -0400

    add-component draft v1

--- src/actions/add-component.js
+++ src/actions/add-component.js
@@ -1,5 +1,14 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 const addComponent = (name) => {
-  console.log(name);
+  console.log(`Installing: ${name}... please wait`);
+
+  const pathName = path.resolve(`../components/${name}.tsx`);
+
+  fs.writeFile(pathName, "todo").then(() => {
+    console.log(`${name}: successfully installed`);
+  });
 };
 
 module.exports.addComponent = addComponent;

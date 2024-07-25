f3afa3fcf11ea3b9fd81d10ae5ae727daba8eaa2
learnuidev <learnuidev@gmail.com>
2024-07-25 18:37:45 -0400

    added index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { listDirectoryNames } = require("./actions/list-directory-names");
+const { getComponent } = require("./actions/get-component");
+const { addComponent } = require("./actions/add-component");
+
+module.exports = {
+  listDirectoryNames,
+  getComponent,
+  addComponent,
+};

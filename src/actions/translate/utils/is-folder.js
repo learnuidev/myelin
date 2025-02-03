42c765f4b776bab8d5168df0b7d51cfb8e2f6904
learnuidev <learnuidev@gmail.com>
2025-02-03 12:08:34 -0500

    handle edge case for isFolder

--- src/actions/translate/utils/is-folder.js
+++ src/actions/translate/utils/is-folder.js
@@ -1,8 +1,13 @@
 const fs = require("fs/promises");
 
 async function isFolder(path) {
-  const stats = await fs.stat(path);
-  return stats.isDirectory();
+  try {
+    const stats = await fs.stat(path);
+    return stats.isDirectory();
+    // eslint-disable-next-line no-unused-vars
+  } catch (err) {
+    return false;
+  }
 }
 
 module.exports = {

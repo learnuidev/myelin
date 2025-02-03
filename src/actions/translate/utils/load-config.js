fed749a14144575a78eca576be171beca6e6f731
learnuidev <learnuidev@gmail.com>
2025-02-03 13:11:38 -0500

    better error message

--- src/actions/translate/utils/load-config.js
+++ src/actions/translate/utils/load-config.js
@@ -2,10 +2,8 @@ const { readFile } = require("./read-file");
 
 async function loadConfig() {
   // step 1: read config
-  const config = await readFile("myelin.config.json");
 
-  if (!config) {
-    throw new Error(`config not found. Please create a file called: myelin.config.json in root directory and fill in this info:
+  const errorMessage = `config not found. Please create a file called: myelin.config.json in root directory and fill in this info:
 {
   "aiProvider": "openai",
   "aiModel": "gpt-4o-mini",
@@ -15,10 +13,19 @@ async function loadConfig() {
     "targetLanguages": ["es", "fr", "zh"]
   }
 }
-`);
-  }
+`;
+  try {
+    const config = await readFile("myelin.config.json");
 
-  return JSON.parse(config);
+    if (!config) {
+      throw new Error(errorMessage);
+    }
+
+    return JSON.parse(config);
+    // eslint-disable-next-line no-unused-vars
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 }
 
 module.exports = {

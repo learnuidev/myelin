aacb35dde9a2c9a72a8dd5079eb1ae569d424db1
learnuidev <learnuidev@gmail.com>
2025-02-04 14:03:50 -0500

    fix load config

--- src/actions/translate/utils/load-config.js
+++ src/actions/translate/utils/load-config.js
@@ -1,25 +1,40 @@
 const { readFile } = require("./read-file");
 
+const fs = require("fs").promises;
+
+const defaultConfig = {
+  aiProvider: "openai",
+  aiModel: "gpt-4o-mini",
+  locale: {
+    location: "locales",
+    sourceLanguage: "en",
+    targetLanguages: ["es", "fr", "zh"],
+  },
+};
+
 async function loadConfig() {
   // step 1: read config
 
   const errorMessage = `config not found. Please create a file called: myelin.config.json in root directory and fill in this info:
-{
-  "aiProvider": "openai",
-  "aiModel": "gpt-4o-mini",
-  "locale": {
-    "location": "locales",
-    "sourceLanguage": "en",
-    "targetLanguages": ["es", "fr", "zh"]
-  }
-}
+  
+  ${JSON.stringify(defaultConfig)}
 `;
+
+  const configPath = "myelin.config.json";
   try {
-    const config = await readFile("myelin.config.json");
+    let config;
+    config = await readFile(configPath);
 
     if (!config) {
-      throw new Error(errorMessage);
+      console.log(`${configPath} not found. Writing a default one`);
+      await fs.writeFile(configPath, JSON.stringify(defaultConfig));
+
+      console.log(`${name}: successfully installed`);
+
+      // throw new Error(errorMessage);
     }
+
+    config = await readFile(configPath);
 
     return JSON.parse(config);
     // eslint-disable-next-line no-unused-vars

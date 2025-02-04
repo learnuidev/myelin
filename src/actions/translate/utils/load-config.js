d671ca1bf21b1556cf973da02c351218bf746704
learnuidev <learnuidev@gmail.com>
2025-02-04 15:59:07 -0500

    wip

--- src/actions/translate/utils/load-config.js
+++ src/actions/translate/utils/load-config.js
@@ -4,22 +4,26 @@ const fs = require("fs").promises;
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const defaultConfig = {
-  aiProvider: "openai",
-  aiModel: "gpt-4o-mini",
-  locale: {
-    location: "locales",
-    sourceLanguage: "en",
-    targetLanguages: ["es", "fr", "zh"],
+const defaultConfig = JSON.stringify(
+  {
+    aiProvider: "openai",
+    aiModel: "gpt-4o-mini",
+    locale: {
+      location: "locales",
+      sourceLanguage: "en",
+      targetLanguages: ["es", "fr", "zh"],
+    },
   },
-};
+  null,
+  4
+);
 
 async function loadConfig() {
   // step 1: read config
 
   const errorMessage = `config not found. Please create a file called: myelin.config.json in root directory and fill in this info:
   
-  ${JSON.stringify(defaultConfig)}
+  ${defaultConfig}
 `;
 
   const configPath = "myelin.config.json";
@@ -28,16 +32,23 @@ async function loadConfig() {
 
     config = await readFile(configPath, { throw: false });
 
-    if (!config) {
-      console.log(`${configPath} not found. Writing a default one`);
-      await fs.writeFile(configPath, JSON.stringify(defaultConfig, null, 4));
-
-      console.log(`${name}: successfully installed`);
+    if (config) {
+      console.log("Config found");
     }
 
-    config = await defaultConfig;
+    if (!config) {
+      console.log(`${configPath} not found. Writing a default one`);
+      await fs.writeFile(configPath, defaultConfig);
 
-    return JSON.parse(config);
+      config = defaultConfig;
+    }
+
+    if (config) {
+      return JSON.parse(config);
+    } else {
+      return JSON.parse(defaultConfig);
+    }
+
     // eslint-disable-next-line no-unused-vars
   } catch (err) {
     throw new Error(errorMessage);

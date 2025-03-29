2982d3f6da9dee8417551e4c1e91f7cbd5c9023d
learnuidev <learnuidev@gmail.com>
2025-03-28 20:25:13 -0400

    fixed typo

--- src/components/i18next/index.js
+++ src/components/i18next/index.js
@@ -22,56 +22,56 @@ const i18next = {
         ],
         codes: [
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/cookie-utils.ts",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/cookie-utils.ts",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/cookie-utils.ts",
           },
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/cookies.ts",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/cookies.ts",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/cookies.ts",
           },
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/get-lang-and-direction.ts",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/get-lang-and-direction.ts",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/get-lang-and-direction.ts",
           },
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/i18n-config.ts",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/i18n-config.ts",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/i18n-config.ts",
           },
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/i18n-next-html-provider.tsx",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/i18n-next-html-provider.tsx",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/i18n-next-html-provider.tsx",
           },
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/i18n-next-provider.tsx",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/i18n-next-provider.tsx",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/i18n-next-provider.tsx",
           },
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/init.ts",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/init.ts",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/init.ts",
           },
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/language-switcher.tsx",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/language-switcher.tsx",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/language-switcher.tsx",
           },
           {
-            targetDir: "src/libs/i18-next/",
-            path: "src/libs/i18-next/use-translation.tsx",
+            targetDir: "src/libs/i18n-next/",
+            path: "src/libs/i18n-next/use-translation.tsx",
             codeUrl:
               "https://raw.githubusercontent.com/learnuidev/mamachef/refs/heads/main/src/libs/i18n-next/use-translation.tsx",
           },

cad4504c4c1b4b874e6a680c37d618e055dfcb05
learnuidev <learnuidev@gmail.com>
2025-04-23 12:20:56 -0400

    fix bug

--- src/actions/translate/create-translation-service.js
+++ src/actions/translate/create-translation-service.js
@@ -1,6 +1,12 @@
 const createTranslationService = (translationRepository) => {
-  const translate = async ({ sourceTranslation, config, targetLanguage }) => {
+  const translate = async ({
+    fileLocation,
+    sourceTranslation,
+    config,
+    targetLanguage,
+  }) => {
     const resp = await translationRepository.translate({
+      fileLocation,
       sourceTranslation,
       config,
       targetLanguage,

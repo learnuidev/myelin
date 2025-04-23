29c6fa606ffd6659f3dc4dc3195f8d413793dc5a
learnuidev <learnuidev@gmail.com>
2025-04-23 16:03:34 -0400

    real time google

--- src/actions/translate/repositories/google-repository.js
+++ src/actions/translate/repositories/google-repository.js
@@ -1,9 +1,20 @@
 const { translateTo } = require("../../../../lib/google/translate-to");
 const { loadApiKey } = require("../utils/load-api-key");
+const { loadTranslation } = require("../utils/load-translation");
+const { writeJsonFile } = require("../utils/write-json-file");
 
 const googleRepository = () => {
-  const translate = async ({ sourceTranslation, config, targetLanguage }) => {
-    let res = {};
+  const translate = async ({
+    fileLocation,
+    sourceTranslation,
+    config,
+    targetLanguage,
+  }) => {
+    let originalExistingTranslation = await loadTranslation(fileLocation);
+
+    let res = {
+      ...originalExistingTranslation,
+    };
     const xApiKey = await loadApiKey({ config, targetLanguage });
 
     for (const keyVal of Object.entries(sourceTranslation)) {
@@ -16,6 +27,8 @@ const googleRepository = () => {
       });
 
       res[key] = resp;
+
+      await writeJsonFile(fileLocation, res);
     }
 
     return res;
